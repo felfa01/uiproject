1468af7ea6bb2d61f7a978c06e118f1ff59cca3f
majkh <holmberg.majk@gmail.com>
2016-03-03 22:47:50 +0100

    added credit and user

--- javascript/accounts.js
+++ javascript/accounts.js
@@ -1,40 +1,89 @@
+
+var creditAdd = 0;
 $( document ).ready(function() {
 	getUsers(urlParams["username"], urlParams["password"]);
-		$(".close").on("click", function(){
-		$(".modal").css('display','none');
+	$(".close").on("click", function(){
+		$(".modal").css('display','none');	
+		$('form').trigger("reset");	
+	});
+	window.onclick = function(event) {
+		if (event.target == document.getElementById('usrModal') || event.target == document.getElementById('creditModal') ) {
+			$(".modal").css('display','none');
+			$('form').trigger("reset");
+			creditAdd = 0;
+		}
+	}
+	$( ".usrForm" ).submit(function( event ) {
+		event.preventDefault();
+		$.ajax({
+			url: "http://pub.jamaica-inn.net/fpdb/api.php?username="+urlParams["username"]
+			+"&password="+urlParams["password"]+"&new_username="+$("input[name='new_username']").val()+
+			"&new_password="+$("input[name='new_password']").val()+"&first_name="+$("input[name='first_name']").val()+
+			"&last_name="+$("input[name='last_name']").val()+"&email="+$("input[name='email']").val()+
+			"&phone="+$("input[name='phone']").val()+"&action=user_edit",
+			type: 'post',
+			success: function(data) {
+				if(data.type === "error"){
+					alert("error!!")
+				}else{
+					alert(data);
+					$('form').trigger("reset");
+					$(".modal").css('display','none');
+					getUsers(urlParams["username"], urlParams["password"]);
+				}
+			}	
+		});
+	});
 
-    //modal.style.display = "none";
+	$("input[name='add_credit']").bind('input', function() { 
+		if($(this).val() != ""){
+			$("input[name='new_credit']").val(parseFloat($(this).val()) + parseFloat(creditAdd));
+		}else{
+			$("input[name='new_credit']").val(creditAdd);
+		}
+	});
 });
-		window.onclick = function(event) {
-	if (event.target == document.getElementById('usrModal')) {
-		$(".modal").css('display','none');
-        //modal.style.display = "none";
-    }
-}
-});
+
+
+
 
 function getUsers(username, pwd){
+	$("#user-table tbody").html("")
 	url = "http://pub.jamaica-inn.net/fpdb/api.php?username="+username+"&password="+pwd+"&action=iou_get_all";
 	$.getJSON( url, {
 		format: "json"
 	}).done(function(data) {
 		data.payload.sort(SortByName);
 		jQuery.each(data.payload, function(i, usr) {
-			$("#user-table tbody").append("<tr id='"+ i +"'><td>"+ usr.username +"</td><td>"+ usr.first_name
-				+"</td><td>"+ usr.last_name +"</td><td>"+ usr.assets +"</td><td><i id='editBtn' class='fa fa-pencil-square-o'></i></td></tr>");
+			$("#user-table tbody").append("<tr id='"+ i +"'><td id='username'>"+ usr.username +"</td><td id='firstname'>"+ usr.first_name
+				+"</td><td id='lastname'>"+ usr.last_name +"</td><td id='assets'>"+ usr.assets 
+				+"</td><td><i class='editUsr fa fa-pencil-square-o'></i><i class='addCredit fa fa-credit-card-alt'></i><i class='deleteUsr fa fa-user-times'></i></td></tr>");
 		});
-		$("#editBtn").on("click", function(){
+		$(".editUsr").on("click", function(){
+			$("input[name='new_username']").val($(this).closest("tr").children("#username").html());
+			$("input[name='first_name']").val($(this).closest("tr").children("#firstname").html());
+			$("input[name='last_name']").val($(this).closest("tr").children("#lastname").html());
+			
 			$('#usrModal').css('display','block');
 		});
-	});
+		$(".addCredit").on("click", function(){
+			$("input[name='new_credit']").val($(this).closest("tr").children("#assets").html());
+			creditAdd =	$(this).closest("tr").children("#assets").html();
+			$("input[name='add_credit']").val("0");		
+			$('#creditModal').css('display','block');
+		});
+		$(".deleteUsr").on("click", function(){
+			confirm("Arrr!! Are you sure you want to delete seaman " + $(this).closest("tr").children("#firstname").html() + " " +
+				$(this).closest("tr").children("#lastname").html() + "?")
+		});
 
+	});
 }
 
-    function SortByName(x,y) {
-    	var a = x.last_name.toLowerCase() + x.first_name.toLowerCase();
-    	var b = y.last_name.toLowerCase() + y.first_name.toLowerCase();
-      return ((a == b) ? 0 : ((a > b) ? 1 : -1 ));
-    }
-
+function SortByName(x,y) {
+	var a = x.last_name.toLowerCase() + x.first_name.toLowerCase();
+	var b = y.last_name.toLowerCase() + y.first_name.toLowerCase();
+	return ((a == b) ? 0 : ((a > b) ? 1 : -1 ));
+}
 
 
