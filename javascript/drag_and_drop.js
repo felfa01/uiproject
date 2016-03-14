8df9ea2b3ce9fb65ab30da03b4a32b65f3df87a4
majkh <holmberg.majk@gmail.com>
2016-03-14 07:19:44 +0100

    changes to drag and drop

--- javascript/drag_and_drop.js
+++ javascript/drag_and_drop.js
@@ -1,8 +1,28 @@
+
+
+function makeDragable(){
+	var nodeList = document.getElementsByClassName('draggable')
+
+	for(var i=0; i < nodeList.length; i++){
+		var obj = nodeList[i];
+		obj.addEventListener('touchmove', function(event){
+			var touch = event.targetTouches[0];
+			allowDrop(event);
+			event.target.style.left = touch.pageX + 'px';
+			event.target.style.top = touch.pageY + 'px';
+			console.log(event.target.style.top)
+			event.preventDefault();
+		}, false);
+	}
+} 
+
+
 function allowDrop(ev) {
 	ev.preventDefault();
 }
 
 function drag(ev) {
+	console.log("DRAG STARTED")
 	ev.dataTransfer.setData("text", ev.target.id);
 }
 
@@ -11,23 +31,26 @@ function drop(ev) {
 	var data = ev.dataTransfer.getData("text");
 	var exist = checkExist(data);
 	if(!exist){
-		var element = document.getElementById(data).cloneNode(true);
-		ev.target.appendChild(document.getElementById(data).cloneNode(true));
+		addElement(ev, data);
+		//ev.target.appendChild(document.getElementById(data).cloneNode(true));
 	}else{
 
 	}
 }
 
+function addElement(ev, beer_id){
+	var element = document.getElementById(beer_id).cloneNode(true);
+	//var newElement = document.createElement("LI"); 
+	//newElement.append("<span><span id='counter'>1</span>")
+	$(ev.target).append("<li id='" + beer_id + "'><span>" + $(element).find('#brand').html() + "</span><span id='counter'>1</span></li>");
+
+}
 
 function checkExist(beer_id){
 	var existed=false;
 	$('#order-list li').each(function(){
 		if(beer_id == $(this).attr("id")){
-			if($(this).find("#counter").length){
-				$(this).find("#counter").html(parseInt($(this).find("#counter").html()) + 1)
-			}else{
-				$(this).append("<span id='counter'>1</span>")
-			}
+			$(this).find("#counter").html(parseInt($(this).find("#counter").html()) + 1)
 			existed = true;
 		}
 	});
--- javascript/order_page.js
+++ javascript/order_page.js
@@ -8,20 +8,59 @@ $( document ).ready(function() {
 
 function getBeers(username, pwd){
 
-		$("#stock-table tbody").html("")
+	$("#stock-table tbody").html("")
 	url = "http://pub.jamaica-inn.net/fpdb/api.php?username="+username+"&password="+pwd+"&action=inventory_get";
 	$.getJSON( url, {
 		format: "json"
 	}).done(function(data) {
 		data.payload.sort(SortByName);
 		jQuery.each(data.payload, function(i, beer) {
-			if(beer.namn != "" && beer.namn2 !="")
-		$('#beer-list').append("<li id='"+ beer.beer_id + "' draggable='true' ondragstart='drag(event)'>" + beer.namn +"</li>")
+			if(beer.namn == "" && beer.namn2 ==""){
+				i -= 1;
+			}else{
+				if((i%3) == 0){
+					$('#beer-table tbody').append("<tr><td class='draggable beer-element' id='"+ beer.beer_id + 
+						"' draggable='true' ondragstart='drag(event)' tabindex='" + i +"'><img src='../images/beer.png'></img>"+
+						"<ul><li id='brand'>" +
+						((beer.namn2 != "")? beer.namn + ", " + beer.namn2: beer.namn) +"</li><li id='price'>"+ beer.price + "</li></ul></td></tr>");
+				}else{
+					$('#beer-table tbody tr').last().append("<td class='draggable beer-element' id='"+ beer.beer_id +
+						"' draggable='true' ondragstart='drag(event)' tabindex='" + i +"'><img src='../images/beer.png'></img>"+
+						"<ul><li id='brand'>" +
+						((beer.namn2 != "")? beer.namn + ", " + beer.namn2: beer.namn) +"</li><li id='price'>"+ beer.price + "</li></ul></td>");
 
+				}
+			}
+		});
+		$(".beer-element").focus(function() {
+	fillCollapse($(this), $(this).attr("id"))
+});
+		makeDragable();
 	});
 
-	});
+}
 
+
+
+function fillCollapse($target, beer_id){
+
+$.ajax({
+		url: "http://pub.jamaica-inn.net/fpdb/api.php?username="+urlParams["username"]
+		+"&password="+urlParams["password"]+"&beer_id="+beer_id+"&action=beer_data_get",
+		type: 'post',
+		success: function(data) {
+			if(data.type === "error"){
+				alert(data.payload[0].msg)
+			}else{
+				$(".beer-collapse").remove();
+				console.log(data.payload[0])
+				var beer = data.payload[0];
+				$target.parents('tr').after("<tr class='beer-collapse'><td><img src='../images/beer.png'></img></td><td><ul><li>"+ 
+					((beer.namn2 != "")? data.payload[0].namn + ", " + beer.namn2: beer.namn) + "</li><li>"+ 
+					beer.alkoholhalt + "</li><li>"+ beer.prisinklmoms +"</li></ul></td></tr>");
+			}
+		}	
+	});
 }
 
 
